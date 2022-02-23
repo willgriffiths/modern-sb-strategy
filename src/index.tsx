760e05b7056ba8ace8f64a8030a1cd3e8020bc90
Will Griffiths <thiswill@gmail.com>
2022-02-23 11:00:56 +1000

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+import Chart from "./Chart";
+
+const rootElement = document.getElementById("root");
+render(<Chart />, rootElement);
